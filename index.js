1997417db775f4ba9728ac0bca841e6417840186
Samiul Alim <xo.ot2001@gmail.com>
2022-05-30 20:16:10 +0600

    fixed Cors Issue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import Search from "./pokedex/pokedex.js";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('**POKE-SEARCH Server Running**')
